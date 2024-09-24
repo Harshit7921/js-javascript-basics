2bc6c5eb922d80b6f972ad4ed901bcb0764af10a
HARSHITT SINGH <115341570+Harshit7921@users.noreply.github.com>
2024-09-24 19:58:57 +0530

    Add files via upload
    .

---
+++ 2.variables.js
@@ -0,0 +1,19 @@
+const accountId=94549
+let accountEmail="harshitsingh@gmail.com"
+var accountPassword="67890"
+accountcity="Hapur"
+
+//accountId = 2
+
+accountemail="singh7921@gmail.com"
+accountpassword="99680"
+accountcity="GHAZIABAD"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope.
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountcity]);
