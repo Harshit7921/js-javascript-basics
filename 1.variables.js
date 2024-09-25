df34b1ebc81371ab85fb119080679b510b2d85d8
HARSHITT SINGH <115341570+Harshit7921@users.noreply.github.com>
2024-09-25 18:45:06 +0530

    Add files via upload

---
+++ 1.variables.js
@@ -0,0 +1,20 @@
+const accountId=94549
+let accountEmail="harshitsingh@gmail.com"
+var accountPassword="67890"
+accountcity="Hapur"
+let accountstate;
+
+//accountId = 2 //not allow
+
+accountemail="singh7921@gmail.com"
+accountpassword="99680"
+accountcity="GHAZIABAD"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope.
+*/
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountstate]);
---
+++ 2.datatypes.js
@@ -0,0 +1,31 @@
+"Use strict"; //treat all js code as newer version
+
+//alert(3+3)// we are using node,not browser
+
+console.log(3+
+    
+    3) //code raliblity should high 
+
+console.log("harshit singh")
+
+
+let name ="HARSHIT SINGH"
+let age = 18
+let isloggedIn= false
+let state;
+
+
+// number=>2 to power 53 
+// bigint---     basically used in trading and stocks.
+// string=> ""
+// boolean=> true/false
+// null=> standalone value
+// undefined=>
+// symbol it is used to find uniquenese
+
+
+//object 
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
